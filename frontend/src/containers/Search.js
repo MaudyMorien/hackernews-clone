ba323a2c7b98c3afcca5e9ee2836d3d8bc1b671a
Maudy <maudymorien@hotmail.nl>
2019-05-17 16:23:24 +0200

    filter debug

--- frontend/src/containers/Search.js
+++ frontend/src/containers/Search.js
@@ -27,42 +27,39 @@ const FEED_SEARCH_QUERY = gql`
 `
 class Search extends Component {
 
-  state = {
-    links: [],
-    filter: ''
-  }
+    state = {
+        links: [],
+        filter: ''
+    }
 
-  _executeSearch = async () => {
-    const { filter } = this.state
-    const result = await this.props.client.query({
-      query: FEED_SEARCH_QUERY,
-      variables: { filter },
-    })
-    const links = result.data.feed.links
-    this.setState({ links })
-  }
-  
-  render() {
-    return (
-      <div>
-        <div>
-          Search
+    render() {
+        return (
+            <div>
+                <div>
+                    Search
           <input
-            type='text'
-            onChange={event => this.setState({ filter: event.target.value })}
-          />
-          <button onClick={() => this._executeSearch()}>OK</button>
-        </div>
-        {this.state.links.map((link, index) => (
-          <Link key={link.id} link={link} index={index} />
-        ))}
-      </div>
-    )
-  }
+                        type='text'
+                        onChange={event => this.setState({ filter: event.target.value })}
+                    />
+                    <button onClick={() => this._executeSearch()}>OK</button>
+                </div>
+                {this.state.links.map((link, index) => (
+                    <Link key={link.id} link={link} index={index} />
+                ))}
+            </div>
+        )
+    }
 
-  _executeSearch = async () => {
-    // ... you'll implement this 🔜
-  }
+    _executeSearch = async () => {
+        const { filter } = this.state
+        const result = await this.props.client.query({
+            query: FEED_SEARCH_QUERY,
+            variables: { filter },
+        })
+        const links = result.data.feed.links
+        this.setState({ links })
+    }
+
 }
 
 export default withApollo(Search)
