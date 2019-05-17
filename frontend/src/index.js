a75bc52c8a81098beb36772f5cfe8ec228db0154
Maudy <maudymorien@hotmail.nl>
2019-05-17 14:47:56 +0200

    debug authlink

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,13 +12,9 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context';
 import { AUTH_TOKEN } from './constants';
 
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
-})
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
 })
 
 const authLink = setContext((_, { headers }) => {
@@ -29,6 +25,11 @@ const authLink = setContext((_, { headers }) => {
       authorization: token? `Bearer ${token}` : ''
     }
   }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 })
 
 ReactDOM.render(
