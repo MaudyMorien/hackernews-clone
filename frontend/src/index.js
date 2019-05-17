f3ea846a5afc250197d529f27726404bc94a753f
Maudy <maudymorien@hotmail.nl>
2019-05-17 16:16:13 +0200

    subscription for more links added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
-
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
@@ -11,11 +13,7 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context';
 import { AUTH_TOKEN } from './constants';
-
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-})
+import { runQuery } from 'apollo-server-core';
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem(AUTH_TOKEN)
@@ -26,6 +24,30 @@ const authLink = setContext((_, { headers }) => {
     }
   }
 })
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
